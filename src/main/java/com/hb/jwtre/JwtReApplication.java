63ab44aa967d1fe6974e02d1e2c3a9e163c096c8
윤호빈 <ad4733@naver.com>
2021-06-01 23:15:07 +0900

    JWT setting

---
+++ src/main/java/com/hb/jwtre/JwtReApplication.java
@@ -0,0 +1,13 @@
+package com.hb.jwtre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hb/jwtre/JwtReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.jwtre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
